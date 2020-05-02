aa2292052276bdaeb1280b103dde6914d845bd6e
Madley Sk <madleysk@gmail.com>
2020-05-02 09:27:44 -0400

    settings file corrected

--- project01/settings.py
+++ project01/settings.py
@@ -133,4 +133,4 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
