df5459554861cf341a37a69716720296db625ac7
Madley <madleysk@gmail.com>
2020-05-06 11:18:32 -0400

    corrected bug in sorting

--- smonitoring/views.py
+++ smonitoring/views.py
@@ -22,12 +22,18 @@ def index(request):
 		return HttpResponse('Request problem')
 	if filtre != None and filtre != 'all':
 		filtre = filtre.upper()
+		try:
+			filtre = Region.objects.get(nom_region=filtre.capitalize())
+		except Region.DoesNotExist:
+			filtre = None
+
 		with connection.cursor() as cursor:
-			cursor.execute("SELECT COUNT(*) as qte,code_site_id,nom from smonitoring_evenement e, smonitoring_site s WHERE (e.code_site_id=s.id) AND (e.status_ev=%s) AND (s.region=%s) GROUP BY e.code_site_id,s.nom ORDER BY qte DESC LIMIT 5", params=['down',filtre.upper()])
+			cursor.execute("SELECT COUNT(*) as qte,code_site_id,nom,nom_region from smonitoring_evenement e, smonitoring_site s, smonitoring_region r WHERE (e.code_site_id=s.id) AND (s.region_id=r.id) AND (e.status_ev=%s) AND (r.nom_region=%s) GROUP BY e.code_site_id,s.nom ORDER BY qte DESC LIMIT 5",params=['down',filtre.nom_region])
 			top_bad_sites = cursor.fetchall()
 			top_bad_sites_formated = []
 			for t in top_bad_sites:
 				top_bad_sites_formated.append({"qte":t[0],"id":t[1],"nom":t[2]})
+			print(top_bad_sites)
 		context= {
 			"page_title":"Site Monitoring - Accueil",
 			"internet_status":{"up":Site.objects.filter(internet="up",region=filtre).count(),"down":Site.objects.filter(internet="down",region=filtre).count()},
@@ -35,7 +41,7 @@ def index(request):
 			"fingerprint_status":{"up":Site.objects.filter(fingerprint="up",region=filtre).count(),"down":Site.objects.filter(fingerprint="down",region=filtre).count()},
 			"recent_events":Evenement.objects.select_related('code_site').filter(code_site__region=filtre).order_by('-date_rap')[:5],
 			"top_bad_sites":top_bad_sites_formated,
-			"filtre":filtre.lower(),
+			"filtre":filtre.nom_region.lower(),
 			# SELECT COUNT(*) qte,code_site_id,nom from smonitoring_evenement e,smonitoring_site s WHERE e.code_site_id=s.id AND status_ev="down" GROUP BY code_site_id ORDER BY qte DESC LIMIT 5;
 		}
 		#print(context['recent_events'].query)
